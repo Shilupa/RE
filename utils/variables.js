3d251eb85b3e72452188272de1508d23340afcfb
shilpasy <shilpa.yadav@metropolia.fi>
2023-02-10 14:00:10 +0200

    common file uploads

---
+++ utils/variables.js
@@ -0,0 +1,5 @@
+const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
+const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+const appId = 'Trinity-recycle-app';
+
+export {baseUrl, uploadsUrl, appId};
