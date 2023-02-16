65ad875ad2e610f84b9ed17bc9d9399a14d8ff6d
Suraj <ranabhatsuraj093@gmail.com>
2023-02-17 00:43:18 +0200

    merging correction and homepage navigation fixed

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import {StatusBar} from 'expo-status-bar';
 import {MainProvider} from './contexts/MainContext';
 import Navigator from './navigators/Navigator';
-import Login from './views/Login';
 
 const App = () => {
   return (
     <MainProvider>
       <StatusBar style="auto" />
-      <Login />
+      <Navigator />
     </MainProvider>
   );
 };
