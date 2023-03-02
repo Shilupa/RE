268f0ef7c259a14912e73e9358313a6eb3e0fa07
shilpasy <shilpa.yadav@metropolia.fi>
2023-03-02 12:33:47 +0200

    resolved the issue with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
+    'next/babel","next/core-web-vitals',
   ],
   globals: {
     Atomics: 'readonly',
